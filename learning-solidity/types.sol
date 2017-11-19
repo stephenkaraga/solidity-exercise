63cc6a55f021fdd1e387884828984576b3121931
Stephen Karaga <stephenkaraga@gmail.com>
2017-11-19 17:14:41 -0500

    Solidity types

---
+++ learning-solidity/types.sol
@@ -0,0 +1,28 @@
+contract owned {
+    bool myBool;
+    uint8 myUint8;
+    uint myUint256;
+    bytes32 myBytes32;
+    bytes myBytes;
+    string myString;
+
+    function owned() {
+        myBool = true;
+        myUint8 = 255;
+        myUint256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+        myBytes32 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+        myString = "ABC";
+    }
+
+    function setMyString(string mystring) {
+        myString = mystring;
+    }
+
+    function getMyBytes32() constant returns (bytes32) {
+        return myBytes32;
+    }
+
+    function getMyUint256() constant returns (uint256) {
+        return myUint256;
+    }
+}
